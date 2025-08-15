a8f4165431fc70d319101eea2898db5d8e9f1893
xing75967 <unsv1985@gmail.com>
2025-08-14 21:43:46 -0600

    test

--- backend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-
-
-export function requireLogin(req, res, next) {
-  if (!req.session.user) {
-    return res.status(401).json({ error: "Unauthorized: Please login." });
-  }
-  next();
-}
--- backend/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-
-
-export function errorHandler(err, req, res, next) {
-  console.error(err);
-  if (res.headersSent) return next(err);
-  res.status(500).json({ error: err.message || "Server Error" });
-}
